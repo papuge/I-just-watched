0bfbef54d1f6be22652e0ba4c4b696ff3bbda8cf
Veronika K <veronikakuzn99@gmail.com>
2019-12-09 14:00:08 +0300

    Feed pagination

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using IJustWatched.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace IJustWatched.Controllers
 {
@@ -23,11 +25,11 @@ namespace IJustWatched.Controllers
         public IActionResult Index()
         {
             List<Review> feedReviews = null;
-            var currentUser = GetCurrentUserAsync();
+            var currentUser = GetCurrentUserAsync().Result;
             if (User.Identity.IsAuthenticated)
             {
                 var subscriptions = _context.Subscriptions
-                    .Where(sub => sub.SubscriberUser.Id == currentUser.Result.Id)
+                    .Where(sub => sub.SubscriberUser.Id == currentUser.Id)
                     .Select(sub => sub.SubscriptionUser);
 
                 feedReviews = _context.Reviews
@@ -41,6 +43,62 @@ namespace IJustWatched.Controllers
             return View(feedReviews);
         }
         
+        [Route("api/feed/{currentPage}")]
+        public async Task<JsonResult> LoadReviewsPage(int currentPage)
+        {
+            if (!HttpContext.User.Identity.IsAuthenticated)
+            {
+                return Json("{}");
+            }
+            
+            var isBackBtn = true;
+            var isNextBtn = true;
+            
+            var currentUser = await GetCurrentUserAsync();
+            var subscriptions = _context.Subscriptions
+                .Where(sub => sub.SubscriberUser.Id == currentUser.Id)
+                .Select(sub => sub.SubscriptionUser);
+
+            var reviewsCount = _context.Reviews.Count(review => subscriptions.Contains(review.Author));
+
+            int maxPage = (int)Math.Ceiling(reviewsCount / 5.0);
+            if (currentPage > maxPage) currentPage = maxPage;
+            if (currentPage < 1) currentPage = 1;
+            
+            if (maxPage == 1)
+            {
+                isBackBtn = false;
+                isNextBtn = false;
+            }
+            else if (currentPage == maxPage)
+                isNextBtn = false;
+            else if (currentPage == 1)
+                isBackBtn = false;
+
+            var pageReviews = await _context.Reviews
+                .Where(review => subscriptions.Contains(review.Author))  
+                .OrderByDescending(r => r.CreationDateTime)
+                .Skip((currentPage - 1) * 5)
+                .Take(5)
+                .Select(r => new
+                {
+                    id = r.Id,
+                    authorId = r.Author.Id,
+                    authorUserName = r.Author.UserName,
+                    filmId = r.ReviewFilm.Id,
+                    filmTitle = r.ReviewFilm.Title,
+                    title = r.ReviewTitle,
+                    date = r.CreationDateTime
+                }).ToListAsync();
+
+            return Json(JsonConvert.SerializeObject(new
+            {
+                isBackBtn,
+                isNextBtn,
+                currentPage,
+                pageReviews
+            }));
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
